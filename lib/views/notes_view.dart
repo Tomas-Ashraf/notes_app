619d69d3460feccdcd0c5657a20b5ab31a983fe1
Tomas-Ashraf <tomasashraf70@gmail.com>
2025-03-28 16:47:07 +0200

    custom app bar & body

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,43 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notes',
+          style: TextStyle(fontSize: 30),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 46,
+                width: 46,
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.05),
+                    borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.search,
+                    size: 28,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: NotesViewBody(),
+    );
   }
 }
