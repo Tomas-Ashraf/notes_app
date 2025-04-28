a480584b70793d65ef19abf781b7251fc05fd8fb
Tomas-Ashraf <tomasashraf70@gmail.com>
2025-04-28 12:06:44 +0300

    add simple bloc observer

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,10 +2,9 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:notes_app/cubits/add_note_cubit.dart';
-import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
+
+import 'package:notes_app/widgets/add_note_bottomSheet.dart';
+
 
 import 'package:notes_app/widgets/notes_view_body.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -65,77 +64,6 @@ class NotesView extends StatelessWidget {
   }
 }
 
-class AddNoteBottomSheet extends StatefulWidget {
-  const AddNoteBottomSheet({super.key});
-
-  @override
-  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
-}
-
-class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          // autovalidateMode: autovalidateMode,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 30, bottom: 15, left: 16, right: 16),
-                child: CustomTextFormField(
-                  label: 'Title',
-                  onSaved: (value) {
-                    title = value;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20, left: 16, right: 16),
-                child: CustomTextFormField(
-                  label: 'Content',
-                  onSaved: (value) {
-                    subTitle = value;
-                  },
-                  minLines: 5,
-                ),
-              ),
-              CustomButton(
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    AddNoteCubit().addNote(
-                      NoteModel(
-                          title: title!,
-                          subTitle: subTitle!,
-                          date: 'date',
-                          color: 1),
-                    );
-                    
-                  } else {
-                    autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 // class AddNoteForm extends StatefulWidget {
 //   const AddNoteForm({
--- lib/widgets/add_note_bottomSheet.dart
+++ lib/widgets/add_note_bottomSheet.dart
@@ -1,77 +1,78 @@
-// ignore_for_file: file_names, unused_local_variable
+// ignore_for_file: file_names
 
-// import 'package:flutter/widgets.dart';
-// import 'package:notes_app/widgets/custom_button.dart';
-// import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/custom_button.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
-// class AddNoteBottomSheet extends StatelessWidget {
-//   const AddNoteBottomSheet({super.key});
+class AddNoteBottomSheet extends StatefulWidget {
+  const AddNoteBottomSheet({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding:
-//           EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-//       child: SingleChildScrollView(
-//         child: AddNoteForm(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
 
-// class AddNoteForm extends StatefulWidget {
-//   const AddNoteForm({
-//     super.key,
-//   });
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey();
 
-//   @override
-//   State<AddNoteForm> createState() => _AddNoteFormState();
-// }
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
-// class _AddNoteFormState extends State<AddNoteForm> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final GlobalKey<FormState> formKey = GlobalKey();
-//     AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-//     String? title, subTitle;
-//     return Form(
-//       key: formKey,
-//       // autovalidateMode: autovalidateMode,
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Padding(
-//             padding:
-//                 const EdgeInsets.only(top: 30, bottom: 15, left: 16, right: 16),
-//             child: CustomTextFormField(
-//               label: 'Title',
-//               onSaved: (value) {
-//                 title = value;
-//               },
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.only(bottom: 20, left: 16, right: 16),
-//             child: CustomTextFormField(
-//               label: 'Content',
-//               onSaved: (value) {
-//                 subTitle = value;
-//               },
-//               minLines: 5,
-//             ),
-//           ),
-//           CustomButton(
-//               onTap: () {
-//                 if (formKey.currentState!.validate()) {
-//                   formKey.currentState!.save();
-//                 } else {
-//                   autovalidateMode = AutovalidateMode.always;
-//                   setState(() {});
-//                 }
-//               },
-//               ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  String? title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          // autovalidateMode: autovalidateMode,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 30, bottom: 15, left: 16, right: 16),
+                child: CustomTextFormField(
+                  label: 'Title',
+                  onSaved: (value) {
+                    title = value;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20, left: 16, right: 16),
+                child: CustomTextFormField(
+                  label: 'Content',
+                  onSaved: (value) {
+                    subTitle = value;
+                  },
+                  minLines: 5,
+                ),
+              ),
+              CustomButton(
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    AddNoteCubit().addNote(
+                      NoteModel(
+                          title: title!,
+                          subTitle: subTitle!,
+                          date: 'date',
+                          color: 1),
+                    );
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
